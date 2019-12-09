184d75388413e7261683a275adb592fe38249b86
sam82520.work <sam82520.work@gmail.com>
2019-12-09 18:20:51 +0800

    新增context練習

---
+++ src/context-api/context.js
@@ -0,0 +1,57 @@
+import React from "react";
+import ErrorBoundary from "../ErrorBoundary";
+// import Button from "./button";
+// import "./theme.scss";
+
+import { ThemeContext, themes } from "./theme-context";
+import ThemeTogglerButton from "./theme-toggle-button";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme: state.theme === themes.dark ? themes.light : themes.dark
+      }));
+    };
+
+    // this.toggleTheme = this.toggleTheme.bind(this);
+
+    // State also contains the updater function so it will
+    // be passed down into the context provider
+    this.state = {
+      theme: themes.light,
+      toggleTheme: this.toggleTheme
+    };
+  }
+
+  componentDidMount() {}
+
+  // toggleTheme() {
+  //   this.setState(state => ({
+  //     theme: state.theme === themes.dark ? themes.light : themes.dark
+  //   }));
+  // }
+
+  render() {
+    // The entire state is passed to the provider
+    return (
+      <ErrorBoundary>
+        <ThemeContext.Provider value={this.state}>
+          <Content />
+        </ThemeContext.Provider>
+      </ErrorBoundary>
+    );
+  }
+}
+
+function Content() {
+  return (
+    <div>
+      <ThemeTogglerButton />
+    </div>
+  );
+}
+
+export default App;
