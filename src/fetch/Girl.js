fe8079ba264ba2cf2e9a14338ad7f7d302b0d695
Sam <sam82520.work@gmail.com>
2019-12-13 07:58:02 +0800

    繼續練習hooks

--- src/fetch/Girl.js
+++ src/fetch/Girl.js
@@ -1,24 +1,43 @@
 import React, { useState, useEffect } from "react";
-import './img.scss';
+import "./img.scss";
 
 export default function Girl() {
   const [img, setImg] = useState([]);
+  const [num, setNum] = useState(10);
+  const [search, setSearch] = useState(1);
+  // const [url, setUrl] = useState(
+  // "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/4/1"
+  // );
 
-  async function fetchData() {
-    const res = await fetch("http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/55");
-
-    res.json().then(res => setImg(res.results));
-  }
+  // function getRandomInt(max) {
+  //   return setTimeout(() => {
+  //     return setNum(Math.floor(Math.random() * Math.floor(max)));
+  //   }, 10);
+  // }
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    async function fetchData() {
+      const res = await fetch(
+        `http://gank.io/api/data/%E7%A6%8F%E5%88%A9/4/${search}`
+      );
 
-  // console.log(img);
+      res
+        .json()
+        .then(res => setImg(res.results))
+        .catch(err => console.log(err));
+    }
+
+    fetchData();
+  }, [search]);
+
   return (
     <div>
+      <input type="text" value={num} onChange={e => setNum(e.target.value)} />
+      <button type="button" onClick={() => setSearch(num)}>
+        Search
+      </button>
+      <br/>
       {img.map((item, index) => {
-        console.log(item.url);
         return <img src={item.url} alt={index} key={item._id} />;
       })}
     </div>
