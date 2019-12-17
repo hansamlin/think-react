205f149c9c239df45b5b79921b4dc837abf4b109
Sam <sam82520.work@gmail.com>
2019-12-17 21:17:51 +0800

    修改文字顏色

--- src/Hooks/computer/Text.js
+++ src/Hooks/computer/Text.js
@@ -6,12 +6,14 @@ const Div = styled.div`
   height: calc(100vh / 5);
   font-size: calc(100vh / 6);
   text-align: right;
+  background-color: rgb(46, 41, 42);
+  color: #ffffff;
 `;
 
 const Text = () => {
   const { text } = useContext(Context);
   console.log("render Text", text);
-  
+
   return (
     <Div>
       <span>{text}</span>
